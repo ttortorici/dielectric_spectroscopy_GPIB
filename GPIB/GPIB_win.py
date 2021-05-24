945e75dbd6c9a2fa5f4eb412ba1fceebc3113fd5
Teddy Tort <etc.tortorici@gmail.com>
2021-05-23 18:54:59 -0600

    gpib updates

--- GPIB/GPIB_win.py
+++ GPIB/GPIB_win.py
@@ -7,6 +7,7 @@ Created on Thu Jun 29 14:31:15 2017
 
 import pyvisa
 import time
+import numpy as np
 
 RQS = (1 << 11)
 SRQ = (1 << 12)
@@ -21,21 +22,31 @@ class GPIB(object):
         self.rm = pyvisa.ResourceManager()
         if addr == 5:
             """addr 5 is for HP4275A"""
-            self.dev = self.rm.open_resource('GPIB0::{}::INSTR'.format(self.addr), read_termination='\r\n')
+            self.dev = self.rm.open_resource('GPIB0::{}::INSTR'.format(self.addr),
+                                             read_termination='\r\n', delay=2)
         else:
-            self.dev = self.rm.open_resource('GPIB0::{}::INSTR'.format(self.addr))
+            self.dev = self.rm.open_resource('GPIB0::{}::INSTR'.format(self.addr), delay=2)
     
     def query(self, msg):
         try:
             return self.dev.query(msg)
         except pyvisa.errors.VisaIOError:
             return 'timed out'
-    
+
+    def query_ascii(self, msg, sep=',', conv='f'):
+        try:
+            return self.dev.query_ascii_values(msg, container=np.array, separator=sep, converter=conv)
+        except pyvisa.errors.VisaIOError:
+            return 'timed out'
+
     def write(self, msg):
         self.dev.write(msg)
         
     def read(self):
-        return self.dev.read()
+        try:
+            return self.dev.read()
+        except pyvisa.errors.VisaIOError:
+            return 'timed out'
         
     def dev_id(self):
         return self.query('*IDN?')
